6a7a7e98a70d77120aff32d24d4d4aa7edd09448
Roman Kiselev <roman.kiselev@stjude.org>
2022-07-18 19:30:33 -0500

    Total rewrite of the firmware. Better data structs

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,108 +2,109 @@
 #include <Arduino.h>
 
 #ifndef _AVR_IOXXX_H_
-#include <avr/iom328.h> // Comment this line out to compile and upload!
+#include <avr/iom328.h>
 #endif
 
+// #include "global_vars.h"
+
+/***************************************
+DEFINITIONS OF DATA TYPES AND STRUCTURES
+***************************************/
+#define uchar unsigned char
 #define VERSION "0.2.0"
 
-#include "global_vars.h"
+#pragma pack(push) /* push current alignment to stack */
+#pragma pack(1)    /* set alignment to 1 byte boundary */
 
-/************
-  INTERRUPTS
-************/
-
-// ISR
-ISR(TIMER1_OVF_vect)
+typedef struct
 {
-  switch (timer1_status)
+  uint8_t addr;
+  uint8_t value;
+} Register;
+
+typedef struct
+{
+  uint8_t active;
+  uint8_t idle;
+} Shutter;
+Shutter g_shutter{0xF, 0};
+
+typedef struct
+{
+  uint16_t timer_period_cts;
+  uint16_t n_frames;            // number of frames
+  uint16_t shutter_delay_cts;   // camera delay, in timer counts
+  uint16_t injection_delay_cts; // injection delay, in timer counts
+} Timer1;
+Timer1 g_timer1{600, 7, 16, 48};
+
+typedef struct
+{
+  uint16_t skip;
+} Timelapse;
+Timelapse g_timelapse{0};
+
+typedef struct
+{
+  uint8_t mask;
+} ALEX;
+ALEX g_ALEX{0};
+
+union Data
+{
+  struct
   {
-  case T1STATUS::SHUTTER_OPENING:
-    ++timer1_status;
+    uint8_t cmd;
 
-    // Fluidic signal goes down
-    PORTC &= ~bit(PORTC4);
-
-    write_shutter();
-
-    // Make sure that the timer output is set to zero
-    reset_OC1A();
-
-    start_timer1();
-    break;
-
-  case T1STATUS::ACQUISITION:; // count pulses, stop the timer when reach n_frames
-    if (++n_acquired_frames >= timer1_cfg.n_frames && timer1_cfg.n_frames > 0)
+    // All members below share the same 9 bytes of memory
+    // Th
+    union
     {
-      ++timer1_status;
+      // R/W - register access
+      Register R;
+      Register W;
 
-      while (TCNT1 < OCR1A)
-      {
-        ; // Wait until timer reaches output compare match to have a clean signal
-      }
+      // S - shutter control
+      Shutter S;
 
-      // Disable OC1A timer output to avoid extra trigger at the end of the last frame
-      DDRB &= ~bit(DDB1);
-    }
-    break;
+      // T - timer configuration and E - exposure time
+      Timer1 T;
 
-  case T1STATUS::LAST_FRAME:
-    // Disable timer 1 interrupts and stop timer
-    TIMSK1 &= ~bit(TOIE1);
-    stop_timer1();
-    break;
-  }
+      // L - timelapse
+      Timelapse L;
 
-  PINB = bit(PINB5);
-}
+      // A - ALEX mask
+      ALEX A;
+    };
+  };
+  uchar bytes[9];
+
+} data;
+
+#pragma pack(pop) /* restore original alignment from stack */
+
+// Bytes in the data frame
+struct Bytes
+{
+  uchar cmd;
+  uchar addr;
+  uchar value;
+};
+
+// Data frame consists of bytes but can be represented as text
+union DataFrame
+{
+  struct Bytes bytes;
+  uchar text[4];
+};
+
+union DataFrame df;
+int charsRead;
+bool up = false;
 
 /************
 PROGRAM LOGIC
 ************/
-
-const unsigned char TCCR1A_default = bit(COM1A1) | bit(WGM11);
-const unsigned char TCCR1B_default = bit(WGM13) | bit(WGM12) | bit(CS12) | bit(CS10);
-
-void write_shutter()
-{
-  PORTC &= ~SHUTTER_MASK;
-  PORTC |= (timer1_status == T1STATUS::STOPPED) ? shutter_idle : shutter_running;
-}
-
-void reset_OC1A()
-{
-  TCCR1A = TCCR1A_default & ~bit(WGM10) & ~bit(WGM11);
-  TCCR1B = TCCR1B_default & ~bit(WGM12) & ~bit(WGM13);
-
-  // Set the OC1A pin to zero (datasheet section 15.7.3)
-  TCCR1C = bit(FOC1A);
-
-  // Restore timer state
-  TCCR1A = TCCR1A_default;
-  TCCR1B = TCCR1B_default;
-}
-
-void start_timer1()
-{
-  OCR1A = max(timer1_cfg.timer_period_cts >> 4, 1); // set pulse duration 1/8 of period
-  ICR1 = max(timer1_cfg.timer_period_cts, 2);       // set timer period
-
-  // Ensure that the delay is at least two counts to avoid overfill
-  // Set the timer/counter value and introduce a delay
-  TCNT1 = timer1_cfg.timer_period_cts - max(timer1_cfg.cam_delay_cts, 2);
-
-  DDRB |= bit(DDB1); // enable timer output
-  GTCCR = 0;         // let it run
-}
-
-void stop_timer1()
-{
-  timer1_status = T1STATUS::STOPPED;
-  write_shutter();
-  DDRB &= ~bit(DDB1);              // disable timer output
-  GTCCR = bit(TSM) | bit(PSRSYNC); // pause the timer
-  reset_OC1A();                    // make sure the OC1A pin state is zero
-}
 
 // the setup function runs once when you press reset or power the board
 void setup()
@@ -111,20 +112,10 @@ void setup()
   Serial.begin(2000000);
   Serial.setTimeout(10); // ms
 
-  // Enable shutter output on PORTC, pins 0-3
-  DDRC |= SHUTTER_MASK;
-
-  // Enable fluidics trigger output on PORTC, pin 4
-  DDRC |= bit(DDC4);
-  PORTC &= ~bit(PORTC4);
-
-  // Disable camera trigger output on PB1 and set it to zero (timer 1)
-  PORTB &= ~bit(PORTB1);
-  DDRB &= ~bit(DDB1);
-
-  stop_timer1();
+  g_shutter.idle = 0;
 }
 
+// the loop function runs over and over again forever
 void loop()
 {
   while (!Serial)
@@ -143,86 +134,21 @@ void loop()
 
   if (Serial.available() > 0)
   {
-    charsRead = Serial.readBytes(data.bytes, 3);
+    charsRead = Serial.readBytes(df.text, 3);
     if (charsRead == 3)
     {
-      switch (data.cmd)
+      switch (df.bytes.cmd)
       {
-      // Write the value to the register with given address
       case 'W':
       case 'w':
-        *((uint8_t *)data.reg_addr) = data.reg_value;
+        // Write the value to the register with given address
+        *((uchar *)df.bytes.addr) = df.bytes.value;
         break;
 
-      // Read the value from the given register
       case 'R':
       case 'r':
-        Serial.write(*((uint8_t *)data.reg_addr));
-        break;
-
-      // Remember the shutter state (keeps only 4 bits)
-      case 'S':
-      case 's':
-        shutter_running = data.shutter_running & SHUTTER_MASK;
-        shutter_idle = data.shutter_idle & SHUTTER_MASK;
-        write_shutter();
-        break;
-
-      // start timer 1
-      case 'T':
-      case 't':
-        if (data.timer_period_cts == 0)
-          break; // don't bother, period is zero
-
-        // Read the remaining data from the packet
-        charsRead = Serial.readBytes(data.bytes_extra, 6);
-
-        // Create a copy of the packet
-        memcpy(&timer1_cfg, &data, sizeof(data));
-
-        // Send trigger to fluidic system
-        PORTC |= bit(PORTC4);
-
-        reset_OC1A();
-
-        timer1_status = T1STATUS::SHUTTER_OPENING;
-        n_acquired_frames = 0;
-
-        // Start timer 1 - waiting for fludic mixing
-        ICR1 = max(timer1_cfg.inj_delay_cts, 2);
-
-        // Enable overflow interrupt (TIMER1_OVF_vect)
-        TIMSK1 |= bit(TOIE1);
-        interrupts();
-
-        // Rest the timer and let it run
-        TCNT1 = 0;
-        GTCCR = 0;
-        break;
-
-      // Change the timer period (camera exposure time)
-      case 'E':
-      case 'e':
-        if (data.timer_period_cts == 0)
-          break; // don't bother, period is zero
-
-        // change the timer period
-        timer1_cfg.timer_period_cts = data.timer_period_cts;
-        // no camera delay - immediate change
-        timer1_cfg.cam_delay_cts = 0;
-        start_timer1();
-        break;
-
-      // Setup timelapse imaging
-      case 'L':
-      case 'l':
-
-        break;
-
-      // stop timer 1
-      case 'Q':
-      case 'q':
-        stop_timer1();
+        // Read the value from the given register
+        Serial.write(*((uchar *)df.bytes.addr));
         break;
       }
     }
